265cfcff08ae676dc975d6082aac59582dd7dff1
Billy Schweigert <billyschweigert@gmail.com>
2019-08-09 16:39:36 -0400

    Adding Gif Support

--- App.js
+++ App.js
@@ -14,13 +14,17 @@ import {
   View,
   Text,
   StatusBar,
+  Image
 } from 'react-native';
 import CardStack, { Card } from 'react-native-card-stack-swiper';
 
 const App = () => {
   return (
     <CardStack style={styles.content} ref={swiper => { this.swiper = swiper }}>
-    <Card style={[styles.card, styles.card1]}><Text style={styles.label}>A</Text></Card>
+    <Card style={[styles.card, styles.card1]}><Image 
+        source={{uri: 'http://www.clicktorelease.com/code/gif/1.gif'}}  
+        style={{width: 100, height:100 }} 
+    /></Card>
     <Card style={[styles.card, styles.card2]}><Text style={styles.label}>B</Text></Card>
     <Card style={[styles.card, styles.card1]}><Text style={styles.label}>C</Text></Card>
   </CardStack>
