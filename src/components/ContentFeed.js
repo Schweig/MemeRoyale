506a757c56cf73322365a936c1460030187384ae
Billy Schweigert <billyschweigert@gmail.com>
2019-12-01 23:54:22 -0500

    voting and feed additions

--- src/components/ContentFeed.js
+++ src/components/ContentFeed.js
@@ -1,133 +1,210 @@
-import React, { Component } from "react";
-import { StyleSheet, View, Image, Text, TouchableOpacity } from "react-native";
-import CardStack, { Card } from "react-native-card-stack-swiper";
-import CardFlip from "react-native-card-flip";
+import React, { Component } from 'react';
+import { StyleSheet, View, Image, Text, TouchableOpacity,ActivityIndicator,StatusBar,AsyncStorage } from 'react-native';
+import CardStack, { Card } from 'react-native-card-stack-swiper';
+import CardFlip from 'react-native-card-flip';
 
 export default class ContentFeed extends Component {
   constructor() {
     super();
-    this.state = { key: 1 };
+    this.state = { key: 1, loading: true };
+
   }
+  componentDidMount() {
+    AsyncStorage.getItem('userToken').then((token) => {
+      console.log(token);
+      this.setState({
+        loading: false,
+        token:token,
+      });
+      this.getFeed();
+    });
+  }
+
+  async postData(url = '', data = {}) {
+    // Default options are marked with *
+    const response = await fetch(url, {
+      method: 'POST', // *GET, POST, PUT, DELETE, etc.
+      mode: 'cors', // no-cors, *cors, same-origin
+      cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+      credentials: 'same-origin', // include, *same-origin, omit
+      headers: {
+        'Content-Type': 'application/json',
+        // 'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      redirect: 'follow', // manual, *follow, error
+      referrer: 'no-referrer', // no-referrer, *client
+      body: JSON.stringify(data), // body data type must match "Content-Type" header
+    });
+    return await response.json(); // parses JSON response into native JavaScript objects
+  }
+
   async getFeed() {
     try {
-      let response = await fetch("http://10.0.2.2:3000/feed");
-      let responseText = await response.text();
-      console.log(responseText);
-      return responseText.movies;
+      let response = await this.postData('http://10.0.2.2:3000/feed', {
+        user:
+          this.state.token,
+      });
+      this.setState({ feed: response });
+      console.log(response);
+      return response;
     } catch (error) {
       console.error(error);
     }
   }
+  async vote(post,direction) {
+    try {
+      let response = await this.postData('http://10.0.2.2:3000/vote', {
+        user:
+          this.state.token,
+        post:post,
+        type:direction,
+      });
+      this.setState({ feed: response });
+      console.log(response);
+      return response;
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
   render() {
-    //this.getFeed();
-    var arr = [
-      "https://www.clicktorelease.com/code/gif/1.gif",
-      "https://www.clicktorelease.com/code/gif/2.gif"
-    ];
-    return (
-      <CardStack
-        style={styles.content}
-        ref={swiper => {
-          this.swiper = swiper;
-        }}
-      >
-        {arr.map((uri, index) => {
-          return (
-            <CardFlip style={ styles.card } ref={ (card) => this['card' + index] = card } >
-            <TouchableOpacity style={ styles.card } onPress={() => this['card' + index].flip()} ><Text>{uri}</Text></TouchableOpacity>
-            <TouchableOpacity style={ styles.card } onPress={() => this['card' + index].flip()} ><Text>{uri}</Text></TouchableOpacity>
-          </CardFlip>
-          );
-        })}
-      </CardStack>
-    );
+    let arr = [];
+    if (this.state.feed) {
+      arr = this.state.feed;
+    }
+
+    if (!this.state.loading) {
+      return (
+        <CardStack
+          style={styles.content}
+          ref={swiper => {
+            this.swiper = swiper;
+          }}
+        >
+          {arr.map((item, index) => {
+            return (
+              <CardFlip
+                style={styles.card}
+                onSwipedRight={() => this.vote(item.id,1)}
+                onSwipedLeft={() => console.log('onSwipedLeft')}
+                key={index}
+                ref={card => (this['card' + index] = card)}
+              >
+                <TouchableOpacity
+                  style={styles.card}
+                  onPress={() => this['card' + index].flip()}
+                >
+                  <Text>{item.title}</Text>
+                </TouchableOpacity>
+                <TouchableOpacity
+                  style={styles.card}
+                  onPress={() => this['card' + index].flip()}
+                >
+                  <Image
+                    style={{ width: '100%', height: 500 }}
+                    source={{ uri: item.link }}
+                  />
+                </TouchableOpacity>
+              </CardFlip>
+            );
+          })}
+        </CardStack>
+      );
+    } else {
+      return (
+        <View>
+          <ActivityIndicator />
+          <StatusBar barStyle="default" />
+        </View>
+      );
+    }
   }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    flexDirection: "column",
-    backgroundColor: "#f2f2f2",
-    width: "100%",
-    height: "100%"
+    flexDirection: 'column',
+    backgroundColor: '#f2f2f2',
+    width: '100%',
+    height: '100%',
   },
   content: {
     flex: 5,
-    alignItems: "center",
-    justifyContent: "center"
+    alignItems: 'center',
+    justifyContent: 'center',
   },
   card: {
     width: 320,
     height: 470,
-    backgroundColor: "#FE474C",
+    backgroundColor: '#FE474C',
     borderRadius: 5,
-    shadowColor: "rgba(0,0,0,0.5)",
+    shadowColor: 'rgba(0,0,0,0.5)',
     shadowOffset: {
       width: 0,
-      height: 1
+      height: 1,
     },
-    shadowOpacity: 0.5
+    shadowOpacity: 0.5,
   },
   card1: {
-    backgroundColor: "#FE474C"
+    backgroundColor: '#FE474C',
   },
   card2: {
-    backgroundColor: "#FEB12C"
+    backgroundColor: '#FEB12C',
   },
   label: {
     lineHeight: 400,
-    textAlign: "center",
+    textAlign: 'center',
     fontSize: 55,
-    fontFamily: "System",
-    color: "#ffffff",
-    backgroundColor: "transparent"
+    fontFamily: 'System',
+    color: '#ffffff',
+    backgroundColor: 'transparent',
   },
   footer: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center"
+    justifyContent: 'center',
+    alignItems: 'center',
   },
   buttonContainer: {
     width: 220,
-    flexDirection: "row",
-    justifyContent: "space-between"
+    flexDirection: 'row',
+    justifyContent: 'space-between',
   },
   button: {
-    shadowColor: "rgba(0,0,0,0.3)",
+    shadowColor: 'rgba(0,0,0,0.3)',
     shadowOffset: {
       width: 0,
-      height: 1
+      height: 1,
     },
     shadowOpacity: 0.5,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-    zIndex: 0
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    zIndex: 0,
   },
   orange: {
     width: 55,
     height: 55,
     borderWidth: 6,
-    borderColor: "rgb(246,190,66)",
+    borderColor: 'rgb(246,190,66)',
     borderWidth: 4,
     borderRadius: 55,
-    marginTop: -15
+    marginTop: -15,
   },
   green: {
     width: 75,
     height: 75,
-    backgroundColor: "#fff",
+    backgroundColor: '#fff',
     borderRadius: 75,
     borderWidth: 6,
-    borderColor: "#01df8a"
+    borderColor: '#01df8a',
   },
   red: {
     width: 75,
     height: 75,
-    backgroundColor: "#fff",
+    backgroundColor: '#fff',
     borderRadius: 75,
     borderWidth: 6,
-    borderColor: "#fd267d"
-  }
+    borderColor: '#fd267d',
+  },
 });
--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,9 +1,34 @@
 import React, { Component } from 'react';
+import {AsyncStorage} from 'react-native';
 import { Content, Form, Item, Input, Label, Button, Text } from 'native-base';
 export default class LoginForm extends Component {
   constructor(props){
     super();
     console.log(props);
+  }
+  async postData(url = '', data = {}) {
+    // Default options are marked with *
+    const response = await fetch(url, {
+      method: 'POST', // *GET, POST, PUT, DELETE, etc.
+      mode: 'cors', // no-cors, *cors, same-origin
+      cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+      credentials: 'same-origin', // include, *same-origin, omit
+      headers: {
+        'Content-Type': 'application/json',
+        // 'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      redirect: 'follow', // manual, *follow, error
+      referrer: 'no-referrer', // no-referrer, *client
+      body: JSON.stringify(data), // body data type must match "Content-Type" header
+    });
+    return await response.json(); // parses JSON response into native JavaScript objects
+  }
+
+  async authenticateUser(){
+    const response = await (this.postData('http://10.0.2.2:3000/auth',{username:this.state.username,password:this.state.password}));
+
+    await AsyncStorage.setItem('userToken',response.token);
+    this.props.navigation.navigate('AuthLoading');
   }
   render() {
     return (
@@ -11,13 +36,13 @@ export default class LoginForm extends Component {
         <Form>
           <Item floatingLabel>
             <Label>Username</Label>
-            <Input />
+            <Input onChangeText={(text)=>{this.setState({'username':text});}} />
           </Item>
           <Item floatingLabel last>
             <Label>Password</Label>
-            <Input secureTextEntry={true} />
+            <Input onChangeText={(text)=>{this.setState({'password':text});}}secureTextEntry={true} />
           </Item>
-          <Button style={{marginTop:20,marginHorizontal:10 }} onPress={()=>{this.props.navigation.navigate('App');}} block primary>
+          <Button style={{marginTop:20,marginHorizontal:10 }} onPress={()=>{this.authenticateUser();}} block primary>
             <Text>Log In</Text>
           </Button>
         </Form>
